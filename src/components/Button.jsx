d6edfa72493db3fa8222905be8b063dc3b8ecfcb
mythily-muthu <mythilimuthu18@gmail.com>
2024-02-15 00:40:10 +0530

    Feat: Added register page Fix: Button component fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const Button = ({ onClick, type, children, className, color = "" }) => {
+const Button = ({
+  onClick,
+  type = "button",
+  children,
+  className,
+  color = "",
+}) => {
   return (
     <button
       className={`${className} ${color} py-2 px-4 border rounded-md  font-semibold focus:outline-none focus:ring focus:border-blue-50 `}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,16 +3,88 @@ import Button from "../components/Button";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import logo from "../assets/vetet_logo.png";
 import sign_up from "../assets/signup.svg";
+import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 
 const Register = () => {
   let navigate = useNavigate();
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  let registerFields = [
+    {
+      name: "firstName",
+      type: "text",
+      label: "Fist Name",
+    },
+    {
+      name: "lastName",
+      type: "text",
+      label: "Last Name",
+    },
+    {
+      name: "dateOfBirth",
+      type: "date",
+      label: "Date of Birth",
+    },
+    {
+      name: "dateOfCommission",
+      type: "date",
+      label: "Date of Commission",
+    },
+
+    {
+      name: "phone",
+      type: "number",
+      label: "Phone",
+      width: "w-[45%]",
+    },
+    {
+      name: "email",
+      type: "text",
+      label: "Email",
+      width: "w-[45%]",
+    },
+    {
+      name: "password",
+      type: "password",
+      label: "Password",
+      width: "w-[90%]",
+    },
+  ];
+
+  let registerSchema = Yup.object().shape({
+    firstName: Yup.string().required("First name is required"),
+    lastName: Yup.string().required("Last name is required"),
+    dateOfBirth: Yup.string().required("Date of Birth is required"),
+    dateOfCommission: Yup.string().required("Date of Commission is required"),
+    phone: Yup.string().required("Phone is required"),
+    email: Yup.string().required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  let formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      dateOfCommission: "",
+      phone: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: registerSchema,
+    onSubmit: handleSubmit,
+  });
+  console.log(formik.errors, formik.touched);
+
   const handleLogin = () => {
     navigate("/authentication");
   };
   return (
     <div className="w-full h-screen flex flex-col  items-center">
-      <div className="w-[60%] flex flex-col justify-center items-center  h-full ">
+      <div className="w-full p-5 md:w-[60%] flex flex-col justify-center items-center  h-full ">
         <div>
           <Button
             onClick={handleLogin}
@@ -24,18 +96,51 @@ const Register = () => {
             Back to Login
           </Button>{" "}
         </div>
-        <div className="w-full  border flex justify-center items-center my-5 flex-col p-7 gap-y-5 rounded-md bg-white">
+        <div className="w-full  border flex justify-center  my-5 flex-col md:flex-row p-7 gap-y-5 rounded-md bg-white">
           {/* left */}
-          <div className="flex w-[50%] flex-col gap-y-5">
-            <div className="flex w-[150px] h-14">
-              <img src={logo} alt="logo" />
+          <div className="flex w-[50%] flex-col gap-y-5 items-center">
+            <div className="flex w-[120px] md:w-[150px] h-14">
+              <img
+                src={logo}
+                alt="logo"
+                className="object-contain w-full h-full "
+              />
             </div>
             <div className="flex w-[250px] h-[250px]">
               <img src={sign_up} alt="image" className="w-full h-full" />
             </div>
           </div>
           {/* right */}
-          
+
+          <div className="flex w-full  md:w-[50%] flex-col gap-2 ">
+            <div className="flex gap-2 flex-wrap">
+              {registerFields.map(
+                ({ name, type, label, width = " w-full md:w-[45%]" }) => {
+                  return (
+                    <div className={`flex flex-col gap-2 ${width}`}>
+                      <p>{label}</p>
+                      <input
+                        value={formik.values[name]}
+                        name={name}
+                        type={type}
+                        className="border p-2"
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors[name] && (
+                        <p className="text-red-500 ">{formik.errors[name]}</p>
+                      )}
+                    </div>
+                  );
+                }
+              )}
+            </div>
+            <Button
+              onClick={formik.handleSubmit}
+              className="bg-btn-blue text-white"
+            >
+              Register
+            </Button>
+          </div>
         </div>
       </div>
     </div>
