795597228c16c4a25a9eb6f9e4ac04c8ef006c1f
mythily-muthu <mythilimuthu18@gmail.com>
2024-02-14 11:38:31 +0530

    Feat: Added Folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/";
+
+
 function App() {
-  return <div className="App">dc</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authentication" element={<Login />} />
+          <Route path="/authentication/sign-up" element={<Register/>} />
+          <Route path="/authentication/forgot-password" element={<ForgotPassword/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
